6fc892243088b0066b9a91e18003b60cccd3a130
Gui Silva <adrianon10@gmail.com>
2023-09-01 04:09:19 -0300

    feat: add setErrorHandler

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from '@fastify/cors'
 import 'dotenv/config'
 
 import { userRoutes } from './routes/user'
+import { AppError } from './errors/app-error'
 
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
@@ -12,6 +13,23 @@ app.register(cors, {
 })
 
 app.register(userRoutes)
+
+app.setErrorHandler(function (error, request, reply) {
+  if (error instanceof AppError) {
+    // Log error
+    console.log(error)
+    // Send error response
+    reply.status(error.statusCode!).send({ error: error.message })
+  } else if (error instanceof Fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
+    // Log error
+    console.log(error)
+    // Send error response
+    reply.status(error.statusCode!).send({ error: error.message })
+  } else {
+    // fastify will use parent error handler to handle this
+    reply.status(500).send({ error: error.message })
+  }
+})
 
 app
   .listen({
