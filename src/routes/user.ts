4363b376d2b1f44bde70ac70914df23ed0b6e348
Gui Silva <adrianon10@gmail.com>
2023-09-07 05:15:08 -0300

    chore: add new folder to gitignore

--- src/routes/user.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify'
-
-export const userRoutes = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get('/', (req, res) => {
-    res.send('Hello World User')
-  })
-}
