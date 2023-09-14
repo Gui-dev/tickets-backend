b43f0e58b46f4456eb290b2195a42fbcc76a4d74
Gui Silva <adrianon10@gmail.com>
2023-09-14 04:39:07 -0300

    feat: add findByLocationAndDate contract

--- src/contracts/repositories/event-repository.ts
+++ src/contracts/repositories/event-repository.ts
@@ -1,6 +1,8 @@
 import { Event } from '@prisma/client'
 import { ICreateEvent } from '../../dtos/create-event'
+import { IFindByLocationAndDate } from '../../dtos/find-by-location-and-date'
 
 export interface IEventRepository {
   create(data: ICreateEvent): Promise<Event>
+  findByLocationAndDate(data: IFindByLocationAndDate): Promise<Event[] | null>
 }
