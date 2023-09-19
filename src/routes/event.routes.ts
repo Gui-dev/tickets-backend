e70a94f4a7402b6157ee14b2278baba392845563
Gui Silva <adrianon10@gmail.com>
2023-09-19 04:13:45 -0300

    feat add new route

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from 'fastify'
 
 import { EventController } from './../controllers/event-controller'
+import { EventCategoryController } from '../controllers/event-category-controller'
 
 const eventController = new EventController()
+const eventCategoryController = new EventCategoryController()
 
 export const eventRoutes = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post('/events', eventController.store)
   fastify.get('/events', eventController.show)
+  fastify.get('/events/category/:category', eventCategoryController.show)
 }
