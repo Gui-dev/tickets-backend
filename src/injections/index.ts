f0da172100a129a1fa55ac3e3e24378a49ac91a5
Gui Silva <adrianon10@gmail.com>
2023-09-21 04:52:35 -0300

    feat: add dependency injections

--- src/injections/index.ts
+++ src/injections/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe'
 import { EventRepository } from './../repositories/event-repository'
 import { IEventRepository } from './../contracts/repositories/event-repository'
 
+import { IGetCityByCoordinatesProvider } from './../providers/contracts/get-city-by-coordinates-provider'
+import { GetCityByCoordinatesProvider } from '../providers/get-city-by-coordinates-provider'
+
 container.registerSingleton<IEventRepository>(
   'EventRepository',
   EventRepository,
+)
+
+container.registerSingleton<IGetCityByCoordinatesProvider>(
+  'GetCityByCoordinatesProvider',
+  GetCityByCoordinatesProvider,
 )
