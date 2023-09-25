80c262ce4a0e894664426a7c3e134a8a156dd0ca
Gui Silva <adrianon10@gmail.com>
2023-09-25 04:13:59 -0300

    feat: add find event by id use case

---
+++ src/use-cases/find-event-by-id.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from 'tsyringe'
+import { Event } from '@prisma/client'
+
+import { IEventRepository } from '../contracts/repositories/event-repository'
+import { IFindEventByIdDTO } from '../dtos/find-event-by-id-dto'
+import { AppError } from '../errors/app-error'
+
+@injectable()
+export class FindEventById {
+  // eslint-disable-next-line prettier/prettier
+  constructor(@inject('EventRepository') private eventRepository: IEventRepository,) { }
+
+  public async execute({ id }: IFindEventByIdDTO): Promise<Event> {
+    const event = await this.eventRepository.findEventById({ id })
+
+    if (!event) {
+      throw new AppError('Event not found', 404)
+    }
+
+    return event
+  }
+}
