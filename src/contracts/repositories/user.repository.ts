f1281df89af63bbf89fc0867ecc1022a6ed740a6
Gui Silva <adrianon10@gmail.com>
2023-09-28 04:18:38 -0300

    feat: add helper method findUserByEmail

--- src/contracts/repositories/user.repository.ts
+++ src/contracts/repositories/user.repository.ts
@@ -3,4 +3,5 @@ import { ICreateUserDTO } from '../../dtos/create-user-dto'
 
 export interface IUserRepository {
   create(data: ICreateUserDTO): Promise<User>
+  findUserByEmail(email: string): Promise<User | null>
 }
--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -4,6 +4,16 @@ import { ICreateUserDTO } from '../dtos/create-user-dto'
 import { prisma } from '../services/prisma'
 
 export class UserRepository implements IUserRepository {
+  public async findUserByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
   public async create({ name, email }: ICreateUserDTO): Promise<User> {
     const user = await prisma.user.create({
       data: {
