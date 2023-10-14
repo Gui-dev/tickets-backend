79a03294666574693c159e7382abf5fa3e02f349
Gui Silva <adrianon10@gmail.com>
2023-10-14 02:56:31 -0300

    feat: add new route

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -4,11 +4,13 @@ import { EventController } from './../controllers/event-controller'
 import { EventCategoryController } from '../controllers/event-category-controller'
 import { EventLocationController } from '../controllers/event-location-controller'
 import { ParticipantsController } from '../controllers/participants-controller'
+import { EventNameController } from '../controllers/event-name-controller'
 
 const eventController = new EventController()
 const eventCategoryController = new EventCategoryController()
 const eventLocationController = new EventLocationController()
 const participantsController = new ParticipantsController()
+const eventNameController = new EventNameController()
 
 export const eventRoutes = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post('/events', eventController.store)
@@ -16,6 +18,8 @@ export const eventRoutes = async (fastify: FastifyInstance): Promise<void> => {
   fastify.get('/events/:id', eventController.show)
   fastify.get('/events/location', eventLocationController.show)
   fastify.get('/events/category/:category', eventCategoryController.show)
+  fastify.get('/events/filter', eventCategoryController.show)
+  fastify.post('/events/filter/name', eventNameController.index)
 
   fastify.post('/events/:id/participants', participantsController.store)
 }
